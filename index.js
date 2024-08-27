42049d81a27831471e048559d2b0b48bed2a131f
Saurabh Singh <saurabh_xcz@yahoo.co.in>
2024-08-27 23:38:13 +0530

    update fix server issue

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express= require ('express');
 const port = 8000; 
 const app = express();
-app.get('', function(request, response){
-    response.send("Hello there!");
+app.get('/', function(request, response){
+    response.send("Hello Shalini the paglu!");
 });
+
+app.post('addemployee', function(){} );
 
 app.listen(port, function(){
     console.log("App is listening " + port);
