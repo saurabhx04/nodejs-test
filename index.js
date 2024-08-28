47a42a1f8457ff64540db4efacf82ac4a0172a0b
Saurabh Singh <saurabh_xcz@yahoo.co.in>
2024-08-29 01:38:41 +0530

    update index

--- index.js
+++ index.js
@@ -2,21 +2,30 @@
 const express= require ('express');
 const port = 8000; 
 const app = express();
+const router = express.Router();
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.get('/', (req, res) => {
+
+router.get('/', (req, res) => {
     res.send("Hello Shalini the paglu!");
 });
 
+router.get('/aboutus', (req, res) => {
+    res.send("About us route");
+});
+
+
 //app.post('addemployee', () => {} );
 
-app.post('/addemployee', (req, res) => {
+router.post('/addemployee', (req, res) => {
     let empName = req.body.empName;
     let emppass = req.body.empPass;
     res.end(`Psot succcess, you sent ${empName} and ${emppass}, thanks! `)
 });
 
+app.use('/', router);
 app.listen(port, () => {
     console.log(`App is listening on ${port}`);
 });
