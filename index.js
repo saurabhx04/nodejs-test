1ee3ac54709a8f64235f7b4b1bbaa08c5d609112
Saurabh Singh <saurabh_xcz@yahoo.co.in>
2024-08-29 16:03:35 +0530

    add routes

--- index.js
+++ index.js
@@ -3,27 +3,13 @@ const express= require ('express');
 const port = 8000; 
 const app = express();
 const router = express.Router();
+const routes = require('./routes/routes');
+//const routes = require('./routes/routes');
+routes(router)
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-
-router.get('/', (req, res) => {
-    res.send("Hello there");
-});
-
-router.get('/aboutus', (req, res) => {
-    res.send("About us route");
-});
-
-
-//app.post('addemployee', () => {} );
-
-router.post('/addemployee', (req, res) => {
-    let empName = req.body.empName;
-    let emppass = req.body.empPass;
-    res.end(`Psot succcess, you sent ${empName} and ${emppass}, thanks! `)
-});
 
 app.use('/', router);
 app.listen(port, () => {
