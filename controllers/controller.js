820eb95faf0f38d01ad405ee7da174705d25a3ec
Saurabh Singh <saurabh_xcz@yahoo.co.in>
2024-08-29 20:01:43 +0530

    update routes and controlers

---
+++ controllers/controller.js
@@ -0,0 +1,17 @@
+exports.getdefaults = (req, res) => {
+    res.send('Hello from Saurabh');
+};
+
+exports.aboutus = (req, res) => {
+    res.send('You are on about us route');
+};
+
+exports.addemployees = (req, res) => {
+    let empName = req.body.empName;
+        let empPass = req.body.empPass;
+        res.end(`Psot succcess, you sent ${empName} and ${empPass}, thanks! `)
+};
+
+exports.getemployees = (req, res) => {
+    res.send('You are on the getemployee route')
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,8 @@
+const controller = require('../controllers/controller');
+
 module.exports = function(router){
-    router.get('/', (req, res) => {
-        res.send("Hello there");
-    });
-    
-    router.get('/aboutus', (req, res) => {
-        res.send("About us route");
-    });
-    
-    
-    //app.post('addemployee', () => {} );
-    
-    router.post('/addemployee', (req, res) => {
-        let empName = req.body.empName;
-        let empPass = req.body.empPass;
-        res.end(`Psot succcess, you sent ${empName} and ${empPass}, thanks! `)
-    });
+    router.get('/',controller.getdefaults);
+    router.get('/aboutus',controller.aboutus);
+    router.post('/addemployees',controller.addemployees);
+    router.get('/getemployees',controller.getemployees);
 };
